4f800a7b611e3d1401ebed5de17c22827cb175e7
Ben Carney <ben.carney@mtnsat.com>
2014-11-05 11:39:09 -0800

    Adds phantoms

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,12 @@ apt-get update
 apt-get install -y build-essential libreadline-dev libssl-dev zlib1g-dev libxml2-dev libxslt-dev mongodb-10gen=2.4.11
 apt-get install -y curl wget unzip vim git git-core tmux ruby libsqlite3-dev libmysqlclient-dev nodejs
 
+# Phantomjs
+apt-get install libfontconfig1-dev
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-i686.tar.bz2
+tar -xf phantomjs-1.9.8-linux-i686.tar.bz2
+cp phantomjs-1.9.8-linux-i686/bin/phantomjs /usr/local/bin/
+chmod +x /usr/local/bin/phantomjs
+rm -rf phantomjs*
+
 su -c "source /vagrant/synced_folder/scripts/user-config.sh" vagrant
